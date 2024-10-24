3eb8024212842fab5f87a392f11a9ca318f6662a
agray <agray@qa.com>
2024-10-24 20:02:48 +0100

    EDIT: Car.java - overengineer the hell outta this

--- src/main/java/com/lbg/Main.java
+++ src/main/java/com/lbg/Main.java
@@ -1,6 +1,5 @@
 package com.lbg;
-import com.lbg.classes.Car;
-import com.lbg.classes.CarColour;
+import com.lbg.classes.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -8,19 +7,19 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!\n");
         Car bartCar = new Car();
         bartCar.setColour(CarColour.RED);
-        bartCar.setModel("Blah");
+        bartCar.setMake(CarMake.HONDA);
+        bartCar.setModel(CarModel.JAZZ);
         System.out.println(bartCar.getSpeed());
         System.out.println(bartCar.getColour());
         System.out.println(bartCar.getModel());
-        bartCar.accelerate(30);
+        bartCar.accelerate(30, CarSpeedUnit.MPH);
         System.out.println(bartCar.getSpeed());
-        bartCar.decelerate(10);
+        bartCar.decelerate(10, CarSpeedUnit.MPH);
         System.out.println(bartCar.getSpeed());
-        Car lisaCar = new Car("Blah2");
-        lisaCar.accelerate(40);
+        Car lisaCar = new Car(CarModel.ASTRA, CarMake.VAUXHALL);
+        lisaCar.accelerate(40, CarSpeedUnit.KPH);
         System.out.println(lisaCar.getSpeed());
         Car[] cars = new Car[3];
         cars[0] = bartCar;
